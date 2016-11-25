ea2e505185965a87627bd9fb3dcebf6d60d42b04
girish946 <girish946@gmail.com>
2016-11-25 17:37:23 +0530

    basic functionality done

--- cmdhandler/RCHandle.go
+++ cmdhandler/RCHandle.go
@@ -1,4 +1,4 @@
-package gorcontrol
+package cmdhandler
 
 import (
 	"fmt"
---
+++ server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	rc "github.com/girish946/gorcontrol/cmdhandler"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeHandler)
+	router.HandleFunc("/handle/{command}", handleCommand)
+	fmt.Printf("server starting at http://127.0.0.1:8080\n")
+	http.ListenAndServe(":8080", router)
+
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Info("request from ", r.RemoteAddr, " ", r.URL)
+	data, _ := rc.Read("index.html")
+	fmt.Fprintf(w, data)
+}
+
+func handleCommand(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	log.Info("request from ", r.RemoteAddr, " ", r.URL)
+	commands := map[string]func(){
+		"show_desktop":     rc.ShowDesktop,
+		"show_dashboard":   rc.ShowDashBoard,
+		"alt_tab":          rc.AltTab,
+		"up":               rc.Up,
+		"down":             rc.Down,
+		"left":             rc.Left,
+		"right":            rc.Right,
+		"show_filemanager": rc.ShowFilemanager,
+		"close_Window":     rc.AltF4,
+	}
+	execute, ok := commands[vars["command"]]
+	if ok {
+		execute()
+		fmt.Fprintf(w, "ok")
+	} else {
+		log.Error("command not found")
+		fmt.Fprintf(w, "command not found")
+	}
+}
